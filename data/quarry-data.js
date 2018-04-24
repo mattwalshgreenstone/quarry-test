53cb0796b531dbbd3b602308d5c76aa67c41c703
itomblack <itomblack@hotmail.co.uk>
2018-04-24 09:15:31 -0700

    gh-pages update

---
+++ data/quarry-data.js
@@ -0,0 +1,218 @@
+var members = [
+ {
+   "name": "Ariana de Rhys",
+   "role": "User Experience",
+   "location": "Boulder, CO",
+   "linkedin": "https://www.linkedin.com/in/ariana-de-ryss/",
+   "startdate": "Jul, 2015",
+   "imagefile": "ariana.jpg"
+ },
+ {
+   "name": "Avery Prommer",
+   "role": "Engagement",
+   "location": "Davis, CA",
+   "linkedin": "https://www.linkedin.com/in/avery-prommer-98b45a2/",
+   "startdate": "January, 2017",
+   "imagefile": "averyp.jpg"
+ },
+ {
+   "name": "Carl St Philip",
+   "role": "CFO",
+   "location": "Key West, FL",
+   "linkedin": "https://www.linkedin.com/in/carl-st-philip-10900b7/",
+   "startdate": "October 2017",
+   "imagefile": "carls.jpg"
+ },
+ {
+   "name": "Chance Woodward",
+   "role": "Engagement",
+   "location": "Denver, CO",
+   "linkedin": "https://www.linkedin.com/in/chancewoodward/",
+   "startdate": "Nov, 2015",
+   "imagefile": "chance.jpg"
+ },
+ {
+   "name": "Charles Law",
+   "role": "User Experience",
+   "location": "Toronto, ON",
+   "linkedin": "https://www.linkedin.com/in/charleslaw/",
+   "startdate": "Apr, 2014",
+   "imagefile": "charles.jpg"
+ },
+ {
+   "name": "Chelsea Janssen",
+   "role": "Engagement",
+   "location": "Mineapolis, MN",
+   "linkedin": "https://www.linkedin.com/in/chelsea-b-janssen-90552411/",
+   "startdate": "June 2017",
+   "imagefile": "chelseaj.jpg"
+ },
+ {
+   "name": "Corey Szopinski",
+   "role": "cs",
+   "location": "Boulder, CO",
+   "linkedin": "https://www.linkedin.com/in/coreyszopinski/",
+   "startdate": "January 2018",
+   "imagefile": "coreys.jpg"
+ },
+ {
+   "name": "Darren Himebrook",
+   "role": "Engagement",
+   "location": "New York, NY",
+   "linkedin": "https://www.linkedin.com/in/darrenhimebrook/",
+   "startdate": "March 2016",
+   "imagefile": "darrenh.jpg"
+ },
+ {
+   "name": "David Hyung",
+   "role": "Design",
+   "location": "New York, NY",
+   "linkedin": "https://www.linkedin.com/in/davidhyung/",
+   "startdate": "-",
+   "imagefile": "davidh.jpg"
+ },
+ {
+   "name": "Davis Kramer",
+   "role": "User Experience",
+   "location": "New York, NY",
+   "linkedin": "https://www.linkedin.com/in/davis-kramer-28238220/",
+   "startdate": "-",
+   "imagefile": "davisk.jpg"
+ },
+ {
+   "name": "Ian Black ",
+   "role": "User Experience",
+   "location": "Vancouver, BC",
+   "linkedin": "www.linkedin.com/in/itomblack",
+   "startdate": "Feb, 2018",
+   "imagefile": "ian.jpg"
+ },
+ {
+   "name": "Inah Mercedes",
+   "role": "User Experience",
+   "location": "Miami, FL",
+   "linkedin": "https://www.linkedin.com/in/inahmercedes/",
+   "startdate": "Feb, 2014",
+   "imagefile": "inah.jpg"
+ },
+ {
+   "name": "Jenna Gold",
+   "role": "Engagement",
+   "location": "Denver, CO",
+   "linkedin": "https://www.linkedin.com/in/jennaegold/",
+   "startdate": "-",
+   "imagefile": "jennag.jpg"
+ },
+ {
+   "name": "Jeremy Hamman",
+   "role": "Design",
+   "location": "New York, NY",
+   "linkedin": "https://www.linkedin.com/in/fatandhandsome/",
+   "startdate": "March 2017",
+   "imagefile": "jeremyh.jpg"
+ },
+ {
+   "name": "Jordan Fripp",
+   "role": "Design",
+   "location": "Atlanta, GA",
+   "linkedin": "https://www.linkedin.com/in/jordanfripp/detail/photo/",
+   "startdate": "-",
+   "imagefile": "jordanf.jpg"
+ },
+ {
+   "name": "Joshua Bletterman",
+   "role": "Creative",
+   "location": "Barbados, CAR",
+   "linkedin": "https://www.linkedin.com/in/joshuabletterman/",
+   "startdate": "Jun, 2015",
+   "imagefile": "joshb.jpg"
+ },
+ {
+   "name": "Kathryn White",
+   "role": "Operations",
+   "location": "Boulder, CO",
+   "linkedin": "https://www.linkedin.com/in/kathryn-white-08502499/",
+   "startdate": "Oct, 2016",
+   "imagefile": "kathrynw.jpg"
+ },
+ {
+   "name": "Lou Thor",
+   "role": "Operations",
+   "location": "Denver, CO",
+   "linkedin": "https://www.linkedin.com/in/louthor/",
+   "startdate": "-",
+   "imagefile": "lout.jpg"
+ },
+ {
+   "name": "Mara Walsh",
+   "role": "-",
+   "location": "Boulder, CO",
+   "linkedin": "https://www.linkedin.com/in/mara-walsh-64a4527/",
+   "startdate": "-",
+   "imagefile": "maraw.jpg"
+ },
+ {
+   "name": "Matt Walsh",
+   "role": "CEO",
+   "location": "Boulder, CO",
+   "linkedin": "https://www.linkedin.com/in/mattwalsh/",
+   "startdate": "Jan, 2014",
+   "imagefile": "matt.jpg"
+ },
+ {
+   "name": "Sarah Barrow",
+   "role": "User Experience",
+   "location": "Denver, CO",
+   "linkedin": "https://www.linkedin.com/in/scbarrow/",
+   "startdate": "Dec, 2014",
+   "imagefile": "sarah.jpg"
+ },
+ {
+   "name": "Scott Potter",
+   "role": "Engagement",
+   "location": "Boulder, CO",
+   "linkedin": "https://www.linkedin.com/in/scottrpotter/",
+   "startdate": "April 2017",
+   "imagefile": "scottp.jpg"
+ },
+ {
+   "name": "Sean Cardiff",
+   "role": "User Experience",
+   "location": "Calgary, AB",
+   "linkedin": "https://www.linkedin.com/in/seancardiff/",
+   "startdate": "November, 2016",
+   "imagefile": "seanca.jpg"
+ },
+ {
+   "name": "Sean Curren",
+   "role": "User Experience",
+   "location": "Portland, ME",
+   "linkedin": "https://www.linkedin.com/in/srcurran/detail/photo/",
+   "startdate": "-",
+   "imagefile": "seancur.jpg"
+ },
+ {
+   "name": "Sophia Prater",
+   "role": "User Experience",
+   "location": "Atlanta, GA",
+   "linkedin": "https://www.linkedin.com/in/sophiav/",
+   "startdate": "Jan, 2015",
+   "imagefile": "sophiap.jpg"
+ },
+ {
+   "name": "Tara Greer",
+   "role": "CXO",
+   "location": "Los Angeles, CA",
+   "linkedin": "https://www.linkedin.com/in/taragreer/",
+   "startdate": "Oct, 2017",
+   "imagefile": "tara.jpg"
+ },
+ {
+   "name": "Ted Ripple",
+   "role": "Design",
+   "location": "Los Angeles, CA",
+   "linkedin": "https://www.linkedin.com/in/tedripple/",
+   "startdate": "March 2017",
+   "imagefile": "tedr.jpg"
+ }
+]
---
+++ js/cards-2.js
@@ -0,0 +1,199 @@
+
+$(document).ready(function() {
+
+	var wrapper = document.getElementById('wrapper');
+	var header = document.getElementById('header');
+	var filterSection = document.getElementById('filter-section');
+	var filter = 'none';
+	var searchbox = document.getElementById('searchbox');
+	var clearsearch = $('#clear-search');
+
+
+
+	//========================================================
+	// SEARCH
+	//========================================================
+
+	//creates a listener for when you press a key
+	window.onkeyup = keyup;
+	var searchText;
+
+	function keyup(e) { 
+  	//perform search
+  	performSearch( searchText )
+	}
+	
+
+	function  performSearch( searchText ) {
+
+		//get input text for every key press
+	  searchText = searchbox.value.toLowerCase();
+
+		var results = [];
+		
+		for ( var i=0; i < members.length; i++ ) {
+		  for ( key in members[i] ) {
+		    if ( members[i][key].toLowerCase().indexOf( searchText )!=-1 ) {
+		      results.push( members[i] );
+		      break;
+		    }
+		  }
+		} //end for loop
+		make_cards( results );
+
+	}
+
+	// CLEAR SEARCH
+	clearsearch.click( function(event) {
+		searchbox.value = "";
+		make_cards( members );
+	});
+
+
+	//========================================================
+	//FILTERING
+	//========================================================
+
+	//fill up filters
+	var allRoles = []
+	var filterCode = '';
+	//get all roles
+	members.forEach(function(entry) { allRoles.push(entry.role) });
+
+	//filter for ones more than 1 of
+	var roleFilters = GetPopular(allRoles);
+
+	//sort alphabetically
+	roleFilters.sort((a, b) => a.localeCompare(b))
+	//create DOM item for each (removing empty ones)
+	roleFilters.forEach( function(role) {
+		if ( role !== '-') {
+				var filterItem = '<div id="filter-' + role + '" class="filter">' + role + '</div>'
+				filterCode = filterCode + filterItem;
+		}
+	
+	});	
+
+	//add to DOM
+	$('#filter-wrap').html(filterCode);
+
+	
+
+	$('.filter').click( function(event) {
+
+		// 		var filteredMembers = $.grep(members, function (member, i) {
+		// 			return member.role == filter;
+		// 		});
+
+		// 		make_cards(filteredMembers);
+
+		// fill text box with text
+		searchbox.value = event.target.textContent;
+		performSearch( searchText )
+
+	});
+
+
+	//========================================================
+	//USEFUL FUNCTIONS
+	//========================================================
+
+	function GetUnique( inputArray )
+	{
+		var outputArray = [];
+		for (var i = 0; i < inputArray.length; i++)
+		{
+			if ((jQuery.inArray(inputArray[i], outputArray)) == -1)
+			{
+				outputArray.push(inputArray[i]);
+			}
+		}
+		return outputArray;
+	}
+
+	function GetPopular( inputArray ) {
+
+		//set min value
+		var moreThan = 1;
+
+		var o = inputArray.reduce((o, n) => {
+		  n in o ? o[n] += 1 : o[n] = 1;
+		  return o;
+		}, {});
+
+		var outputArray = Object.keys(o).filter(k => o[k] > moreThan);
+
+
+		return outputArray;
+	}
+
+
+
+	//========================================================
+	//MAKING CARDS
+	//========================================================
+
+	var make_cards = (function(data){
+		//clear html content variable
+		var quarryPeople = "";	
+		
+		data.forEach(function(entry) {
+
+				// Add people to collection
+					//create template
+				var person = 
+				'<div class="info-card">'
+				 	+ '<img id="profile-pic" class="profile-img" src="img/people/' + entry.imagefile + '" alt="profile-pic"></img>'
+					+ '<div class="card-text">' 
+						+ '<h2 class="profile-name">' + entry.name + '</h2>'
+						+ '<h5>Department</h5><h3 class="role">' + entry.role + '</h3>'
+						+ '<h5>Location</h5><h3 id="location">' + entry.location + '</h3>';
+
+					if (entry.linkedin != '') {
+						person = person	+ '<a class= "linkedin-wrap" href="' + entry.linkedin +  '" target="blank"> <img src="img/icons/linkedin.svg" alt=""></a>'
+					};
+
+					person = person + '</div>'
+					+ '</div>'
+				;
+					//add to collection
+	 			quarryPeople = quarryPeople + person;
+				
+		 });
+
+		//push array of people to DOM
+		$('#card-wrap').html(quarryPeople);
+				
+
+			
+	});
+	//END MAKE CARDS
+
+
+
+	//========================================================
+	// WRAPPER PADDING
+	//========================================================
+
+	$( window ).resize(function() {
+		setWrapperTop()
+	})
+
+	var setWrapperTop = function() {
+		var offset =  filterSection.offsetHeight + header.offsetHeight;
+		wrapper.style.setProperty("top", offset + "px");
+	}
+
+
+	//========================================================
+	//FIRST PAGE RUN
+	//========================================================
+	setWrapperTop();
+	make_cards(members);
+
+
+
+
+      
+});
+
